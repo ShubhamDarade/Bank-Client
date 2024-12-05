d6c1c4bb274d8d1870c8a4362ba79531ba57fbcc
Shubham Darade <shubhamdarade717@gmail.com>
2024-12-05 22:23:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import {
+  InvoiceTab,
+  Login,
+  PageNotFound,
+  ReconciliationPage,
+  Register,
+  ReportSummary,
+  TransactionTab,
+} from "./pages";
+import "./App.css";
+import { Header } from "./components";
+import ProtectedRoutes from "./protectedRoutes/ProtectedRoutes";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Toaster />
+      <Routes>
+        {/* <Route path="/" element={<AllBlogs />} /> */}
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+
+        <Route element={<ProtectedRoutes />}>
+          <Route path="/" element={<InvoiceTab />} />
+          <Route path="/invoice" element={<InvoiceTab />} />
+          <Route path="/transaction" element={<TransactionTab />} />
+          <Route path="/reconcile" element={<ReconciliationPage />} />
+          <Route path="/report" element={<ReportSummary />} />
+          <Route path="/*" element={<PageNotFound />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import Register from "./Register";
+import Login from "./Login";
+import PageNotFound from "./PageNotFound";
+import InvoiceTab from "./Invoice";
+import ReconciliationPage from "./ReconciliationPage";
+import TransactionTab from "./Transaction";
+import ReportSummary from "./Report";
+
+export {
+  Register,
+  Login,
+  PageNotFound,
+  InvoiceTab,
+  ReconciliationPage,
+  TransactionTab,
+  ReportSummary,
+};
